8332255b7260d272e65e769e880c9a57dc604682
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-11 22:57:53 -0400

    scaffold Node + Express backend with TypeScript

---
+++ api/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv"; 
+
+const app = express(); 
+
+const port = process.env.PORT || 5000; 
+
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("PumpAI server is running!"); 
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
