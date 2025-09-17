afc2cb7271140f86e114c14e920f46776bac0510
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-16 23:57:29 -0400

    apply dark theme globally on all routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,21 +7,31 @@ import Progress from "./pages/Progress";
 import Trainer from "./pages/Trainer";
 import NotFound from "./pages/NotFound";
 import AppLayout from "./components/AppLayout";
+import Register from "./pages/Register";
+import { useEffect } from "react";
 
 function App() {
+  // Apply theme settings globally
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+
+    document.documentElement.classList.toggle("dark", savedTheme === "dark");
+  }, []);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<Landing />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/" element={<AppLayout />}>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/workouts" element={<Workouts />} />
             <Route path="/nutrition" element={<Nutrition />} />
             <Route path="/progress" element={<Progress />} />
             <Route path="/trainer" element={<Trainer />} />
-            <Route path="*" element={<NotFound />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
