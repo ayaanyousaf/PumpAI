3120daf32f3adfc6b4331fb02fb4041fbad91753
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-17 23:45:32 -0400

    configure dotenv and add .env file to backend

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,16 @@
 import express, { Request, Response } from "express";
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 
-const app = express(); 
+dotenv.config();
 
-const port = process.env.PORT || 5000; 
+const app = express();
 
-app.use(express.json())
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("PumpAI server is running!"); 
+  res.send("PumpAI server is running!");
 });
 
 app.listen(port, () => {
