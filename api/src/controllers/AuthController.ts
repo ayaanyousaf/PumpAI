9794eb26bf0891dcbf559e5d0b116537f6579170
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-18 05:56:46 -0400

    create AuthController with register logic (validation and hashing)

---
+++ api/src/controllers/AuthController.ts
@@ -0,0 +1,47 @@
+import expressAsyncHandler from "express-async-handler";
+import * as bcrypt from "bcryptjs";
+import User from "../models/UserModel";
+
+// @desk Register a user
+// @route POST /api/auth/register
+// @access public
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields required.");
+  }
+
+  const usernameTaken = await User.findOne({ username });
+  if (usernameTaken) {
+    res.status(400);
+    throw new Error("Username already taken.");
+  }
+
+  const emailTaken = await User.findOne({ email });
+  if (emailTaken) {
+    res.status(400);
+    throw new Error("An account with this email already exists.");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password: ", hashedPassword);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`User ${user} created`);
+
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is invalid.");
+  }
+});
+
+export default registerUser;
