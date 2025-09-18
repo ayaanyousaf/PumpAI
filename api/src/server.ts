98415706550c3918877a6fffcc5efe5e3de2a33e
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-18 06:00:24 -0400

    configure CORS to allow requests from frontend React app

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/dbConnection";
+
+import authRoutes from "./routes/AuthRoutes";
 
 dotenv.config();
 
@@ -7,7 +11,12 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+connectDB();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+
+// Mount routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("PumpAI server is running!");
