d2e22d7c94c0cfd374ddd83cfdca485e3c621e7d
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-18 05:58:57 -0400

    add AuthRoutes with register endpoint

---
+++ api/src/routes/AuthRoutes.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import registerUser from "../controllers/AuthController";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+export default router;
