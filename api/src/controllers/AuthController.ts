c3c957307d22d9c5fbb0122efa0780dcdc4e7975
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-19 21:36:29 -0400

    add login logic to AuthController

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -1,5 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import * as bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/UserModel";
 
 // @desk Register a user
@@ -44,4 +45,40 @@ const registerUser = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export default registerUser;
+// @desk Login a user
+// @route POST /api/auth/login
+// @access public
+const loginUser = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields required.");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "15m" }
+    );
+    res.status(200).json({
+      token: accessToken,
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Email or password is incorrect.");
+  }
+});
+
+export { registerUser, loginUser };
