af2baa9a94c68b24f10940efbfeeb5830efee18e
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-25 23:54:41 -0400

    add reset-password endpoint to AuthRoutes

--- api/src/routes/AuthRoutes.ts
+++ api/src/routes/AuthRoutes.ts
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   forgotPassword,
+  resetPassword,
 } from "../controllers/AuthController";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
