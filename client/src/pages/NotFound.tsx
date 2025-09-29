0315d98b250b7cb8889413a91d4f8a2f6195bcc6
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-28 23:58:49 -0400

    improve 404 Not Found page styling

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col gap-4 bg-primary justify-center items-center w-dvw h-dvh">
+    <div className="flex flex-col gap-4 bg-bg-primary justify-center items-center w-dvw h-dvh">
       <h1 className="text-5xl text-center">Page not found.</h1>
       <h1 className="text-6xl">😕</h1>
-      <Link to="/" className="text-2xl text-secondary underline">
-        Go Home
+      <Link to="/" className="text-2xl text-brand underline">
+        Go home
       </Link>
     </div>
   );
