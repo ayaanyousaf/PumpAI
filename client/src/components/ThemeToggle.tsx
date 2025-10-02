350a888c9c1f9a941c88563ec6a14fcc7ca8d89e
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-10-01 23:51:33 -0400

    refactor ThemeToggle to use ThemeContext for updating global theme

--- client/src/components/ThemeToggle.tsx
+++ client/src/components/ThemeToggle.tsx
@@ -1,38 +1,15 @@
 import { Moon, Sun } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useTheme } from "../context/ThemeContext";
 
 function ThemeToggle() {
-  const [isDarkMode, setDarkMode] = useState(true);
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
-      setDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("theme", "light");
-      setDarkMode(false);
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    if (isDarkMode) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      setDarkMode(false);
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setDarkMode(true);
-    }
-  };
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <button
       onClick={toggleTheme}
       className="z-50 p-2 rounded-full transition-colors duration-300 cursor-pointer"
     >
-      {isDarkMode ? (
+      {theme === "dark" ? (
         <Sun className="h-6 w-6 text-yellow-300" />
       ) : (
         <Moon className="h-6 w-6 text-blue-900" />
